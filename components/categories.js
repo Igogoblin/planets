a936e7f17ff07e1655b4f605b07f4e79265fbaf8
Ihar Skavysh <igogoblin@gmail.com>
2023-05-30 21:22:04 +0300

    fix: chenge availability function

--- components/categories.js
+++ components/categories.js
@@ -11,7 +11,35 @@ panelChoiceFilter.addEventListener("click", () => {
 });
 export function availability() {
   console.log(categoriesLi[1].dataset.categor);
-  // let choiceItem = document.querySelectorAll(".choice-item");
+  let choiceItem = document.querySelectorAll(".choice-item");
+  for (let j = 0; j < categoriesLi.length; j++) {
+    console.log(categoriesLi[j]);
+    console.log(categoriesLi[j].classList.contains("choice-item"));
+    if (categoriesLi[j].classList.contains("choice-item")) {
+      let count = 0;
+      for (let i = 0; i < ourArray.length; i++) {
+        if (ourArray[i].categories == categoriesLi[j].dataset.categor) {
+          count++;
+        }
+      }
+      categorLi[j].textContent = count;
+    } else {
+      categorLi[j].textContent = "";
+    }
+  }
+  for (let i = 0; i < sizeLi.length; i++) {
+    if (sizeLi[i].classList.contains("choice-item")) {
+      let count = 0;
+      for (let j = 0; j < ourArray.length; j++) {
+        if (ourArray[j].size == sizeLi[i].dataset.size) {
+          count++;
+        }
+      }
+      sizeLi[i].textContent = count;
+    } else {
+      sizeLi[i].textContent = "";
+    }
+  }
   // for (let i = 0; i < choiceItem.length; i++) {}
 
   // for (let i = 0; i < categorLi.length; i++) {
@@ -42,7 +70,7 @@ export function availability() {
   //   }
   // }
 }
-function buildSortArray() {
+export function buildSortArray() {
   let choiceItem = document.querySelectorAll(".choice-item");
   let countArray = [];
   console.log(ourArray);
@@ -51,6 +79,8 @@ function buildSortArray() {
     for (let j = 0; j < ourArray.length; j++) {
       if (ourArray[j].categories == choiceItem[i].dataset.categor) {
         countArray.push(ourArray[j]);
+      }
+      if (ourArray[j].size == choiceItem[i].dataset.size) {
       }
     }
   }
